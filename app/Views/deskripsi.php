db6328e0f84225e4ce8adfc7e48174d3b6a1c6c2
php <panjihusnipadhila@gmail.com>
2021-06-20 02:02:48 +0700

    detail opk

--- app/Views/deskripsi.php
+++ app/Views/deskripsi.php
@@ -1,12 +1,22 @@
-<div class="card">
-<?php foreach($opk as $detail): ?>  
-  <div class="card-body">
-    <h5 class="card-title"><?= $detail['nama']?></h5>
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item"><?= $detail['kategori']?></li>
-  </ul>
+<div class="section-container">
+  <div class="container text-left">
+    <div class="card">
+      <?php foreach ($opk as $detail) : ?>
+        <div class="card-body">
+          <h5 class="card-title"><?= $detail['nama'] ?></h5>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item"><?= $detail['kategori']; ?> - <?= $detail['subkategori'] ?></li>
+        </ul>
 
-  <img src="./assets/images/profil-01.jpg" class="card-img-bottom">
-<?php endforeach; ?>
+        <img src="./assets/opk-images/<?= $detail['foto']; ?>" class="card-img-bottom">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item"><?= $detail['deskripsi'] ?></li>
+        </ul>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Kondisi: <?= $detail['kondisi'] ?></li>
+        </ul>
+      <?php endforeach; ?>
+    </div>
+  </div>
 </div>
