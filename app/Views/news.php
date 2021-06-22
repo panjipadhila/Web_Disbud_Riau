e03d7c92815c98960d11bb17a9377a4504301112
mhdguntur <gunturrrok@gmail.com>
2021-06-22 14:41:31 +0700

    header homepage(belum diatur) dan asset img news

--- app/Views/news.php
+++ app/Views/news.php
@@ -3,9 +3,8 @@
     <div class="row">
       <div class="col-xs-12 col-md-8 col-md-offset-2">
         
-        <h1 class="text-center">Moon</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas luctus at sem quis varius.
-          Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus iaculis magna sagittis elit sagittis, at hendrerit lorem venenatis. Morbi accumsan iaculis blandit. Cras ultrices hendrerit nisl.</p>
+        <h1 class="text-center">News</h1>
+        <p class="text-center">Halaman ini memuat berita terkini terkait informasi kebudayaan di Provinsi Riau</p>
 
         <div class="section-container-spacer">
           <img src="./assets/images/img-06.jpg" alt="" class="img-responsive reveal-content" data-action="zoom">
@@ -24,21 +23,41 @@
   </div>
 </div>
 
+<div class="section-container">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-md-8 col-md-offset-2">
+        
 
+        <div class="section-container-spacer">
+          <img src="./assets/images/img-08.jpg" alt="" class="img-responsive reveal-content" data-action="zoom">
+        </div>
+
+        <div class="row">
+          <div class="col-xs-6">
+            <img src="./assets/images/img-09.jpg" alt="" class="img-responsive reveal-content" data-action="zoom">
+          </div>
+          <div class="col-xs-6">
+            <img src="./assets/images/img-10.jpg" alt="" class="img-responsive reveal-content" data-action="zoom">
+          </div>
+          <div class="row">
+          <div class="col-xs-6">
+            <img src="./assets/images/img-11.jpg" alt="" class="img-responsive reveal-content" data-action="zoom">
+          </div>
+          <div class="col-xs-6">
+            <img src="./assets/images/img-12.jpg" alt="" class="img-responsive reveal-content" data-action="zoom">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
 <div class="section-container">
   <div class="container">
     <div class="row section-container-spacer">
       <div class="col-xs-12 col-md-8 col-md-offset-2">
-        <h2 class="text-center">More</h2>
         <div class="section-container-spacer">
-          <p>Praesent at feugiat est, at faucibus ipsum. Aenean condimentum mauris vel malesuada pulvinar. Vestibulum sit amet hendrerit leo, quis vehicula mi.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
         <img src="./assets/images/img-05.jpg" alt="" class="img-responsive reveal-content" data-action="zoom">
       </div>
