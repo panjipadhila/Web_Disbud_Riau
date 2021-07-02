31045cfebf606aed21352c24f12bd3c5e8f4e24c
mhdguntur <gunturrrok@gmail.com>
2021-07-02 20:09:57 +0700

    ganti footer sama view museum

--- app/Views/museum.php
+++ app/Views/museum.php
@@ -1,1 +1,59 @@
-<h2>Data Museum</h2>
+<!-- Add your site or app content here -->
+
+<!-- <div class="hero-full-container background-image-container white-text-container">
+    <div class="container">
+        <div class="row-hero-background">
+            <div class="col-xs-12">
+                <h1>Mountain</h1>
+                <p>WELCOME DINAS KEBUDAYAAN RIAU.</p>
+            </div>
+        </div>
+    </div>
+</div> -->
+
+<div class="section-container">
+    <div class="container text-center">
+        <div class="row section-container-spacer">
+            <div class="col-xs-12 col-md-12">
+                <h2>INVENTARIS MUSEUM</h2>
+            </div>
+        </div>
+        <div class="row">
+            <p><a href="" class="btn btn-primary btn-lg-leftmargin">Objek Museum X</a></p>
+        </div>
+        <div class="row">
+            <p><a href="" class="btn btn-primary btn-lg-leftmargin">Objek Museum X</a></p>
+        </div>
+        <div class="row">
+            <p><a href="" class="btn btn-primary btn-lg-leftmargin">Objek Museum X</a></p>
+        </div>
+        <div class="row">
+            <p><a href="" class="btn btn-primary btn-lg-leftmargin">Objek Museum X</a></p>
+        </div>
+        <div class="row">
+            <p><a href="" class="btn btn-primary btn-lg-leftmargin">Objek Museum X</a></p>
+        </div>
+        <div class="row">
+            <p><a href="" class="btn btn-primary btn-lg-rightmargin">Objek Museum X</a></p>
+        </div>
+        <div class="row">
+            <p><a href="" class="btn btn-primary btn-lg-rightmargin">Objek Museum X</a></p>
+        </div>
+        <div class="row">
+            <p><a href="" class="btn btn-primary btn-lg-rightmargin">Objek Museum X</a></p>
+        </div>
+        <div class="row">
+            <p><a href="" class="btn btn-primary btn-lg-rightmargin">Objek Museum X</a></p>
+        </div>
+        <div class="row">
+            <p><a href="" class="btn btn-primary btn-lg-rightmargin">Objek Museum X</a></p>
+        </div>
+        
+    </div>
+</div>
+
+<!-- <script>
+    document.addEventListener("DOMContentLoaded", function(event) {
+        navbarFixedTopAnimation();
+    });
+</script> -->
