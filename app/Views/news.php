22cd9d952e6825f78a0c445c2dfe3ec5f49dc31d
mhdguntur <gunturrrok@gmail.com>
2021-07-02 15:34:05 +0700

    update layout page news

--- app/Views/news.php
+++ app/Views/news.php
@@ -1,78 +1,45 @@
 <div class="section-container">
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12 col-md-8 col-md-offset-2">
-        
-        <h1 class="text-center">Gallery</h1>
-        <p class="text-center">Halaman ini memuat berita terkini terkait informasi kebudayaan di Provinsi Riau</p>
-
-        <div class="section-container-spacer">
-          <img src="./assets/images/event.jpeg" alt="" class="img-responsive reveal-content" data-action="zoom">
-        </div>
-
-        <div class="row">
-          <div class="col-xs-6">
-            <img src="./assets/images/img-04.jpg" alt="" class="img-responsive reveal-content" data-action="zoom">
-          </div>
-          <div class="col-xs-6">
-            <img src="./assets/images/img-07.jpg" alt="" class="img-responsive reveal-content" data-action="zoom">
-          </div>
-        </div>
-      </div>
-    </div>
+  <div class="row">
+    <div class="col-sm-3">
+    <div class="card" style="width: 40rem fixed;">
+  <img class="card-img-top" src="\assets\images\img-04.jpg" alt="Card image cap" width ="300px">
+  <div class="card-body">
+    <h5 class="card-title">Istana Siak</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>
 
-<div class="section-container">
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12 col-md-8 col-md-offset-2">
-        
-
-        <div class="section-container-spacer">
-          <img src="./assets/images/img-08.jpg" alt="" class="img-responsive reveal-content" data-action="zoom">
-        </div>
-
-        <div class="row">
-          <div class="col-xs-6">
-            <img src="./assets/images/img-09.jpg" alt="" class="img-responsive reveal-content" data-action="zoom">
-          </div>
-          <div class="col-xs-6">
-            <img src="./assets/images/img-10.jpg" alt="" class="img-responsive reveal-content" data-action="zoom">
-          </div>
-          <div class="row">
-          <div class="col-xs-6">
-            <img src="./assets/images/img-11.jpg" alt="" class="img-responsive reveal-content" data-action="zoom">
-          </div>
-          <div class="col-xs-6">
-            <img src="./assets/images/img-12.jpg" alt="" class="img-responsive reveal-content" data-action="zoom">
-          </div>
-        </div>
-      </div>
     </div>
+    <div class="col-sm-3">
+    <div class="card" style="width: 40rem fixed;">
+  <img class="card-img-top" src="\assets\images\img-05.jpg" alt="Card image cap" width ="300px">
+  <div class="card-body">
+    <h5 class="card-title">Joged Sonde</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>
-
-<div class="section-container">
-  <div class="container">
-    <div class="row section-container-spacer">
-      <div class="col-xs-12 col-md-8 col-md-offset-2">
-        <div class="section-container-spacer">
-        </div>
-        <img src="./assets/images/img-05.jpg" alt="" class="img-responsive reveal-content" data-action="zoom">
-      </div>
     </div>
+    <div class="col-sm-3">
+    <div class="card" style="width: 40rem fixed;">
+    <img class="card-img-top" src="\assets\images\img-06.jpg" alt="Card image cap" width ="300px">
+  <div class="card-body">
+    <h5 class="card-title">Tari Cegak</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>
-
-<div class="section-container">
-  <div class="container text-center">
-    <div class="row section-container-spacer">
-      <div class="col-xs-12 col-md-12">
-
-      
-      </div>
     </div>
-
+    <div class="col-sm-3">
+    <div class="card" style="width: 40rem fixed;">
+    <img class="card-img-top" src="\assets\images\img-07.jpg" alt="Card image cap" width ="300px">
+  <div class="card-body">
+    <h5 class="card-title">Tari Cegak</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    </div>
   </div>
 </div>
