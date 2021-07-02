77027a84df938379e325426653ff74a68a28fd97
php <panjihusnipadhila@gmail.com>
2021-07-02 09:50:35 +0700

    fix dropdown header

--- app/Controllers/AdminController.php
+++ app/Controllers/AdminController.php
@@ -23,6 +23,21 @@ class AdminController extends BaseController
         echo view('tambahDataView');
         echo view('footer');
     }
+
+    function save()
+    {
+        $this->DataOpkModel->save([
+            'nama' => $this->request->getVar('nama'),
+            'kategori' => $this->request->getVar('kategori'),
+            'subkategori' => $this->request->getVar('subkategori'),
+            'lokasi' => $this->request->getVar('lokasi'),
+            'kondisi' => $this->request->getVar('kondisi'),
+            'deskripsi' => $this->request->getVar('deskripsi'),
+            'foto' => $this->request->getVar('foto'),
+            'video' => $this->request->getVar('video')
+
+        ]);
+    }
     // function register()
     // {
     //     $data = [
