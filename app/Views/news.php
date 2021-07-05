9614b99537e3bdf96546e2f2c5c0c7cab04b5712
mhdguntur <gunturrrok@gmail.com>
2021-07-05 10:27:46 +0700

    card news

--- app/Views/news.php
+++ app/Views/news.php
@@ -1,16 +1,17 @@
 <div class="section-container">
   <div class="row">
-    <div class="col-sm-3">
-    <div class="card" style="width: 40rem fixed;">
-  <img class="card-img-top" src="\assets\images\img-04.jpg" alt="Card image cap" width ="300px">
-  <div class="card-body">
-    <h5 class="card-title">Istana Siak</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
 
+    <div class="col-sm-3">
+      <div class="card" style="width: 40rem fixed;">
+        <img class="card-img-top" src="\assets\images\img-04.jpg" alt="Card image cap" width ="300px">
+          <div class="card-body">
+            <h5 class="card-title">Istana Siak</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+      </div>
     </div>
+
     <div class="col-sm-3">
     <div class="card" style="width: 40rem fixed;">
   <img class="card-img-top" src="\assets\images\img-05.jpg" alt="Card image cap" width ="300px">
@@ -21,6 +22,7 @@
   </div>
 </div>
     </div>
+
     <div class="col-sm-3">
     <div class="card" style="width: 40rem fixed;">
     <img class="card-img-top" src="\assets\images\img-06.jpg" alt="Card image cap" width ="300px">
@@ -31,6 +33,7 @@
   </div>
 </div>
     </div>
+
     <div class="col-sm-3">
     <div class="card" style="width: 40rem fixed;">
     <img class="card-img-top" src="\assets\images\img-07.jpg" alt="Card image cap" width ="300px">
