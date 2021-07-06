46135aa44892e8eeb0b6cd3d9ab3bf21a2d992df
yudirudja <yudirudja2900@gmail.com>
2021-07-06 09:58:07 +0700

    header update & card carousal

--- app/Views/header.php
+++ app/Views/header.php
@@ -20,19 +20,19 @@
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 
     <link href="/main.550dcf66.css" rel="stylesheet">
     <script src="/DropdownMenu.js"></script>
-    
+
 </head>
 
 <body>
     <!-- Add your content of header -->
     <header>
-        <nav class="navbar-main navbar-main-default active">
+        <nav class="navbar-main navbar-main-fixed-top navbar-main-default active">
             <div class="container">
                 <div class="navbar-main-header">
                     <button type="button" class="navbar-main-toggle collapsed" data-toggle="collapse" data-target="#navbar-main-collapse" aria-expanded="false">
@@ -53,13 +53,13 @@
                 <div class="collapse navbar-main-collapse" id="navbar-main-collapse">
                     <ul class="nav navbar-main-nav navbar-main-right">
                         <li><a href="/" title="">Home</a></li>
-                        <li class ="nav-item dropdown">
-                        <a class="nav-link dropdown toggle" href="#" id = "navbar-mainDropdown" role = "button" data-Toggle="dropdown">Data</a>
-                        <div class = "dropdown-menu" aria-labelledby="navbar-mainDropdown">
-                        <a href="opk" title="">OPK</a>
-                        <br>
-                        <a href="museum" title="">Museum</a>
-                        </div>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown toggle" href="#" id="navbar-mainDropdown" role="button" data-Toggle="dropdown">Data</a>
+                            <div class="dropdown-menu" aria-labelledby="navbar-mainDropdown">
+                                <a href="opk" title="">OPK</a>
+                                <br>
+                                <a href="museum" title="">Museum</a>
+                            </div>
                         </li>
                         <!-- <li><a href="opk" title="">OPK</a></li> -->
                         <li><a href="news" title="">Gallery</a></li>
@@ -74,5 +74,5 @@
                 </div>
             </div>
         </nav>
-        
+
     </header>
--- app/Views/news.php
+++ app/Views/news.php
@@ -1,48 +1,136 @@
-<div class="section-container">
+<section class="container p-t-3">
   <div class="row">
-
-    <div class="col-sm-3">
-      <div class="card" style="width: 40rem fixed;">
-        <img class="card-img-top" src="\assets\images\img-04.jpg" alt="Card image cap" width ="300px">
-          <div class="card-body">
-            <h5 class="card-title">Istana Siak</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
+    <div class="col-lg-12">
+      <h1><span style="color: crimson">Bootstrap 4 Card Slider</span> by webseotips</h1>
+    </div>
+  </div>
+</section>
+<section class="carousel slide" data-ride="carousel" id="postsCarousel">
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-right mb-4">
+        <a class="btn btn-outline-secondary prev" href="" title="go back"><i class="fa fa-lg fa-chevron-left"></i></a>
+        <a class="btn btn-outline-secondary next" href="" title="more"><i class="fa fa-lg fa-chevron-right"></i></a>
       </div>
     </div>
+  </div>
+  <div class="container p-t-0 m-t-2 carousel-inner">
+    <div class="row row-equal carousel-item active m-t-0">
+      <div class="col-md-4">
+        <div class="card">
+          <div class="card-img-top card-img-top-250">
+            <img class="img-fluid" src="img/29223655865_4b8b01989e.jpg" alt="Carousel 1">
+          </div>
+          <div class="card-block p-t-2">
+            <div class="card-header">
+              <h3>flower1</h3>
+            </div>
+            <div class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium
+              animi molestias veritatis, pariatur dolorem error non reprehenderit vero, id. Incidunt
+              hic laudantium soluta recusandae, voluptas libero et! Ipsa, maiores, ratione.</div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card">
+          <div class="card-img-top card-img-top-250">
+            <img class="img-fluid" src="img/35515203036_f8006072b8.jpg" alt="Carousel 1">
+          </div>
+          <div class="card-block p-t-2">
+            <div class="card-header">
+              <h3>flower2</h3>
+            </div>
+            <div class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium
+              animi molestias veritatis, pariatur dolorem error non reprehenderit vero, id. Incidunt
+              hic laudantium soluta recusandae, voluptas libero et! Ipsa, maiores, ratione.</div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card">
+          <div class="card-img-top card-img-top-250">
+            <img class="img-fluid" src="img/3d75927b210409af8582828c4049fcd0.jpg" alt="Carousel 1">
+          </div>
+          <div class="card-block p-t-2">
+            <div class="card-header">
+              <h3>flower1</h3>
+            </div>
+            <div class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium
+              animi molestias veritatis, pariatur dolorem error non reprehenderit vero, id. Incidunt
+              hic laudantium soluta recusandae, voluptas libero et! Ipsa, maiores, ratione.</div>
+          </div>
+        </div>
+      </div>
 
-    <div class="col-sm-3">
-    <div class="card" style="width: 40rem fixed;">
-  <img class="card-img-top" src="\assets\images\img-05.jpg" alt="Card image cap" width ="300px">
-  <div class="card-body">
-    <h5 class="card-title">Joged Sonde</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
     </div>
+    <div class="row row-equal carousel-item m-t-0">
+      <div class="col-md-4">
+        <div class="card">
+          <div class="card-img-top card-img-top-250">
+            <img class="img-fluid" src="img/45590771a1ee18248a49bd399ef99bf1--posts.jpg" alt="Carousel 1">
+          </div>
+          <div class="card-block p-t-2">
+            <div class="card-header">
+              <h3>flower1</h3>
+            </div>
+            <div class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium
+              animi molestias veritatis, pariatur dolorem error non reprehenderit vero, id. Incidunt
+              hic laudantium soluta recusandae, voluptas libero et! Ipsa, maiores, ratione.</div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card">
+          <div class="card-img-top card-img-top-250">
+            <img class="img-fluid" src="img/917d7b69ebd58d0ef0e561cdcb016303--red-rose-bouquet-red-rose-flower.jpg" alt="Carousel 1">
+          </div>
+          <div class="card-block p-t-2">
+            <div class="card-header">
+              <h3>flower1</h3>
+            </div>
+            <div class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium
+              animi molestias veritatis, pariatur dolorem error non reprehenderit vero, id. Incidunt
+              hic laudantium soluta recusandae, voluptas libero et! Ipsa, maiores, ratione.</div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card">
+          <div class="card-img-top card-img-top-250">
+            <img class="img-fluid" src="img/29223655865_4b8b01989e.jpg" alt="Carousel 1">
+          </div>
+          <div class="card-block p-t-2">
+            <div class="card-header">
+              <h3>flower1</h3>
+            </div>
+            <div class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium
+              animi molestias veritatis, pariatur dolorem error non reprehenderit vero, id. Incidunt
+              hic laudantium soluta recusandae, voluptas libero et! Ipsa, maiores, ratione.</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 
-    <div class="col-sm-3">
-    <div class="card" style="width: 40rem fixed;">
-    <img class="card-img-top" src="\assets\images\img-06.jpg" alt="Card image cap" width ="300px">
-  <div class="card-body">
-    <h5 class="card-title">Tari Cegak</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-    </div>
 
-    <div class="col-sm-3">
-    <div class="card" style="width: 40rem fixed;">
-    <img class="card-img-top" src="\assets\images\img-07.jpg" alt="Card image cap" width ="300px">
-  <div class="card-body">
-    <h5 class="card-title">Tari Cegak</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-    </div>
-  </div>
-</div>
+
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+<script>
+  (function($) {
+    "use strict";
+    $('.next').click(function() {
+      $('.carousel').carousel('next');
+      return false;
+    });
+    $('.prev').click(function() {
+      $('.carousel').carousel('prev');
+      return false;
+    });
+  })
+  (jQuery);
+</script>
